4aa4b99a28b02a791e4ba07dcef76f27c81b991c
FJKhan <fjgenus@gmail.com>
2020-07-14 15:33:43 -0500

    refactors sign up method to salt passwords before saving

--- src/api/v1/users/controller.js
+++ src/api/v1/users/controller.js
@@ -1,19 +1,29 @@
+const bcrypt = require('bcryptjs')
 const usersDAO = require('./dao')
 exports.signUp = async (req, res) => {
     try {
         const { email, password } = req.body
         const existingUser = await usersDAO.findOne(email)
+        let newUser
+        let hashedPassword = ''
         //TO-DO:
         // Add error constants
         if (existingUser) {
             res.status(400)
             res.send(new Error(`User account already exists`))
         }
-        const newUser = await usersDAO.create({ email: email, password: password })
-        //TO-DO: assign jwt auth token
-        const { insertedId } = newUser
-        //TO-DO: Standardize successful sign up response
-        res.send({ id: insertedId, email: email })
+        bcrypt
+            .genSalt()
+            .then(async (salt) => {
+                return (hashedPassword = bcrypt.hash(password, salt))
+            })
+            .then(async (hashedPassword) => {
+                newUser = await usersDAO.create({ email: email, password: hashedPassword })
+                //TO-DO: assign jwt auth token
+                const { insertedId } = newUser
+                //TO-DO: Standardize successful sign up response
+                res.send({ id: insertedId, email: email })
+            })
     } catch (e) {
         res.code(500)
         res.send(e)
