24d5850443d2fb34493ebedd32f50023f53d1818
FJKhan <fjgenus@gmail.com>
2020-07-14 23:48:07 -0500

    Adds login functionality. Assigns and returns jwt token on successful sign in. Updates env.example to include jwt secret key and token lifespan examples

--- src/api/v1/users/controller.js
+++ src/api/v1/users/controller.js
@@ -1,32 +1,63 @@
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const usersDAO = require('./dao')
+
+/**
+ * Creates a new user if user with provided email does not already exist
+ * @param {*} req
+ * @param {*} res
+ */
 exports.signUp = async (req, res) => {
     try {
         const { email, password } = req.body
         const existingUser = await usersDAO.findOne(email)
-        let newUser
-        let hashedPassword = ''
-        //TO-DO:
-        // Add error constants
+        //TO-DO: Add error constants
         if (existingUser) {
             res.status(400)
             res.send(new Error(`User account already exists`))
         }
-        bcrypt
-            .genSalt()
-            .then(async (salt) => {
-                return (hashedPassword = bcrypt.hash(password, salt))
-            })
-            .then(async (hashedPassword) => {
-                newUser = await usersDAO.create({ email: email, password: hashedPassword })
-                //TO-DO: assign jwt auth token
-                const { insertedId } = newUser
-                //TO-DO: Standardize successful sign up response
-                res.send({ id: insertedId, email: email })
-            })
+        // hash user password before storing
+        const salt = bcrypt.genSalt()
+        const hashedPassword = await bcrypt.hash(password, salt)
+        const newUser = await usersDAO.create({ email: email, password: hashedPassword })
+        //TO-DO: Standardize successful sign up response
+        res.send({ id: newUser.insertedId, email: email })
     } catch (e) {
-        res.code(500)
+        res.status(500)
         res.send(e)
+        return
+    }
+}
+
+/**
+ * Logs in current user and returns jwt auth token
+ * @param {*} req
+ * @param {*} res
+ * @returns JWT Auth token
+ */
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        //check if user exists
+        const existingUser = await usersDAO.findOne(email)
+        if (!existingUser) {
+            res.status(400)
+            res.send(new Error(`User account doesn't exist`))
+        }
+        const isMatch = await bcrypt.compare(password, existingUser.password)
+        if (isMatch) {
+            // assign jwt auth token
+            const token = await jwt.sign(existingUser, process.env.SECRET_KEY, {
+                expiresIn: process.env.TOKEN_EXPIRES_IN
+            })
+            res.send({ token: token })
+        } else {
+            res.status(400)
+            res.send(new Error(`Password incorrect`))
+        }
+    } catch (error) {
+        res.status(500)
+        res.send(error)
         return
     }
 }
--- src/api/v1/users/routes.js
+++ src/api/v1/users/routes.js
@@ -1,9 +1,7 @@
 const userController = require('./controller')
 
 async function userRoutes(fastify, options) {
-    fastify.post('/users/login', async (request, reply) => {
-        return { hello: 'world' }
-    })
+    fastify.post('/users/login', userController.login)
     fastify.post('/users/sign-up', userController.signUp)
 }
 
