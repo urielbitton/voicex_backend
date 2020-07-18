5f5109f951ce6bd28e56c009a109681e3fd9098a
FJKhan <fjgenus@gmail.com>
2020-07-17 19:06:16 -0500

    Adds forgot password functionality which generates and sends reset password email to user. Adds route to handle forgot password request

--- src/api/v1/users/controller.js
+++ src/api/v1/users/controller.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcryptjs')
+const crypto = require('crypto')
 const jwt = require('jsonwebtoken')
 const usersDAO = require('./dao')
+const emailService = require('../../../services/email/emailService')
 
 /**
  * Creates a new user if user with provided email does not already exist
@@ -19,7 +21,7 @@ exports.signUp = async (req, res) => {
         // hash user password before storing
         const salt = await bcrypt.genSalt()
         const hashedPassword = await bcrypt.hash(password, salt)
-        const newUser = await usersDAO.create({ email: email, password: hashedPassword , createdAt: new Date()})
+        const newUser = await usersDAO.create({ email: email, password: hashedPassword, createdAt: new Date() })
         //TO-DO: Standardize successful sign up response
         res.send({ id: newUser.insertedId, email: email })
     } catch (e) {
@@ -47,7 +49,7 @@ exports.login = async (req, res) => {
         const isMatch = await bcrypt.compare(password, existingUser.password)
         if (isMatch) {
             // assign jwt auth token
-            const token = await jwt.sign(existingUser, process.env.SECRET_KEY, {
+            const token = await jwt.sign({user: existingUser.email}, process.env.SECRET_KEY, {
                 expiresIn: process.env.TOKEN_EXPIRES_IN
             })
             res.send({ token: token })
@@ -62,3 +64,43 @@ exports.login = async (req, res) => {
     }
 }
 
+/**
+ * Sends password reset email to user
+ * @param {*} req
+ * @param {*} res
+ */
+exports.forgotPassword = async (req, res) => {
+    try {
+        const { email } = req.body
+        //check if user exists
+        const existingUser = await usersDAO.findOne(email)
+        if (!existingUser) {
+            res.status(400)
+            res.send(new Error(`User account doesn't exist`))
+        }
+        let token = await createResetPasswordToken(existingUser)
+        let url = createResetPasswordUrl(existingUser, token)
+        let result = await emailService.sendEmail(`fjgenus@gmail.com`, url)
+        res.send({ ok: result })
+    } catch (error) {
+        res.status(500)
+        res.send(error)
+        return
+    }
+}
+
+const createResetPasswordToken = async (user) => {
+    try {
+        const buffer = crypto.randomBytes(20)
+        const payload = buffer.toString('hex')
+        const token = await jwt.sign({user: user._id}, payload, {
+            expiresIn: 3600 // 1 hour
+        })
+        return token
+    } catch (err) {
+        throw new Error(err)
+    }
+}
+const createResetPasswordUrl = (user, token) => {
+    return `http://localhost:3005/user/${user._id}/reset-password?token=${token}`
+}
--- src/api/v1/users/routes.js
+++ src/api/v1/users/routes.js
@@ -3,6 +3,7 @@ const userController = require('./controller')
 async function userRoutes(fastify, options) {
     fastify.post('/users/login', userController.login)
     fastify.post('/users/sign-up', userController.signUp)
+    fastify.post('/users/forgot-password', userController.forgotPassword)
 }
 
 module.exports = userRoutes
