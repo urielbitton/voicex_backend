196ae9b1341f5d52a8dbe8eeb1e3aefc75e3d88a
FJKhan <fjgenus@gmail.com>
2020-07-22 15:32:53 -0500

    updates email service

--- src/services/email/emailService.js
+++ src/services/email/emailService.js
@@ -1,36 +1,34 @@
 const nodemailer = require('nodemailer')
 
-exports.sendEmail = async (email, url) => {
+exports.sendEmail = async (email, subject, html) => {
     try {
         // create reusable transporter object using the default SMTP transport
         let transporter = nodemailer.createTransport({
-            host: process.env.EMAIL_HOST, 
+            host: process.env.EMAIL_HOST,
             port: 465,
-            secure: true, 
+            secure: true,
             requireTLS: true,
             tls: {
                 rejectUnauthorized: false
             },
             auth: {
-                user: process.env.EMAIL_USER, 
-                pass: process.env.EMAIL_PASSWORD, 
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASSWORD
             }
         })
-        let html = `
-        <p>Hey ${email},</p>
-        <p>We heard you're having trouble with your password. Uh-oh.</p>
-        <p>Don’t worry though! You can use the following link to reset your password:</p>
-        <a href=${url}>${url}</a>
-        <p>This link expires in 1 hour. </p>
-        <p>–Your friends at VoiceX</p>
-        `
-
-        let info = await transporter.sendMail({
-            from: process.env.EMAIL_USER,
-            to: email, 
-            subject: 'Reset Password', 
-            html: html // html body
-        })
-        return 1
-    } catch (err) { throw new Error(err)}
+        if (email && subject && html) {
+            await transporter.sendMail({
+                from: process.env.EMAIL_USER,
+                to: email,
+                subject: subject,
+                html: html // html body
+            })
+        }
+        else {
+            throw new Error(`Email parameters not provided`)
+        }
+        return
+    } catch (err) {
+        throw new Error(err)
+    }
 }
