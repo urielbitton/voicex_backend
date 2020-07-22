6ddfe395c7b907c750dc3849abc9840e598b799c
FJKhan <fjgenus@gmail.com>
2020-07-22 15:34:27 -0500

    updates user routes. adds route to manage password reset

--- src/api/v1/users/routes.js
+++ src/api/v1/users/routes.js
@@ -4,6 +4,7 @@ async function userRoutes(fastify, options) {
     fastify.post('/users/login', userController.login)
     fastify.post('/users/sign-up', userController.signUp)
     fastify.post('/users/forgot-password', userController.forgotPassword)
+    fastify.post('/users/reset-password/:email/:token', userController.resetUserPassword)
 }
 
 module.exports = userRoutes
