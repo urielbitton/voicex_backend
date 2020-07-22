e1d5a40b3f260a83bfe01a9846d1682821fb6a54
FJKhan <fjgenus@gmail.com>
2020-07-22 15:33:58 -0500

    updates forgot password function and ands reset password function

--- src/api/v1/users/controller.js
+++ src/api/v1/users/controller.js
@@ -1,8 +1,9 @@
 const bcrypt = require('bcryptjs')
-const crypto = require('crypto')
 const jwt = require('jsonwebtoken')
 const usersDAO = require('./dao')
 const emailService = require('../../../services/email/emailService')
+const resetPasswordUtil = require('../../../utils/resetPassword')
+const templateUtil = require('../../../utils/emailTemplates')
 
 /**
  * Creates a new user if user with provided email does not already exist
@@ -17,6 +18,7 @@ exports.signUp = async (req, res) => {
         if (existingUser) {
             res.status(400)
             res.send(new Error(`User account already exists`))
+            return
         }
         // hash user password before storing
         const salt = await bcrypt.genSalt()
@@ -24,6 +26,7 @@ exports.signUp = async (req, res) => {
         const newUser = await usersDAO.create({ email: email, password: hashedPassword, createdAt: new Date() })
         //TO-DO: Standardize successful sign up response
         res.send({ id: newUser.insertedId, email: email })
+        return
     } catch (e) {
         res.status(500)
         res.send(e)
@@ -45,6 +48,7 @@ exports.login = async (req, res) => {
         if (!existingUser) {
             res.status(400)
             res.send(new Error(`User account doesn't exist`))
+            return
         }
         const isMatch = await bcrypt.compare(password, existingUser.password)
         if (isMatch) {
@@ -53,6 +57,7 @@ exports.login = async (req, res) => {
                 expiresIn: process.env.TOKEN_EXPIRES_IN
             })
             res.send({ token: token })
+            return
         } else {
             res.status(400)
             res.send(new Error(`Password incorrect`))
@@ -77,10 +82,12 @@ exports.forgotPassword = async (req, res) => {
         if (!existingUser) {
             res.status(400)
             res.send(new Error(`User account doesn't exist`))
+            return
         }
-        let token = await createResetPasswordToken(existingUser)
-        let url = createResetPasswordUrl(existingUser, token)
-        let result = await emailService.sendEmail(`fjgenus@gmail.com`, url)
+        let token = await resetPasswordUtil.createResetPasswordToken(existingUser)
+        let url = resetPasswordUtil.createResetPasswordUrl(existingUser, token)
+        let template = templateUtil.getResetPasswordTemplate(existingUser, url)
+        let result = await emailService.sendEmail(existingUser.email, `Reset Password`,template)
         res.send({ ok: result })
     } catch (error) {
         res.status(500)
@@ -89,18 +96,34 @@ exports.forgotPassword = async (req, res) => {
     }
 }
 
-const createResetPasswordToken = async (user) => {
+/**
+ * Resets user's password
+ * @param {*} req 
+ * @param {*} res 
+ */
+exports.resetUserPassword = async (req, res) =>{
     try {
-        const buffer = crypto.randomBytes(20)
-        const payload = buffer.toString('hex')
-        const token = await jwt.sign({user: user._id}, payload, {
-            expiresIn: 3600 // 1 hour
-        })
-        return token
-    } catch (err) {
-        throw new Error(err)
+        const {email, token} = req.params
+        const {password} = req.body
+        let user = await usersDAO.findOne(email)
+        if (!user) {
+            res.status(400)
+            res.send(new Error(`User account doesn't exist`))
+        }
+        if(!token || (user.reset_token !== token)) {
+            res.status(400)
+            res.send(new Error(`Invalid or expired token`))
+        }
+        const salt = await bcrypt.genSalt()
+        const hashedPassword = await bcrypt.hash(password, salt)
+        await usersDAO.update(email, {$set:{password: hashedPassword, reset_token: null}})
+        res.status(202)
+        res.send({success: `Password changed successfully`})
+        return
+        // email notification for successful password reset
+    } catch (error) {
+        res.status(500)
+        res.send(error)
+        return
     }
-}
-const createResetPasswordUrl = (user, token) => {
-    return `http://localhost:3005/user/${user._id}/reset-password?token=${token}`
 }
