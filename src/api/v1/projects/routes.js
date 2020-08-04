4612dcb0d1ffc5c2ede20e3805b56087b6c691f6
FJKhan <fjgenus@gmail.com>
2020-08-03 21:06:22 -0400

    adds update and delete routes for projects

--- src/api/v1/projects/routes.js
+++ src/api/v1/projects/routes.js
@@ -1,6 +1,8 @@
 const projectsController = require('./controller')
 async function projectRoutes(fastify, options) {
     fastify.post('/users/:user_id/projects/', projectsController.create)
+    fastify.put('/users/:user_id/projects/:project_id', projectsController.update)
+    fastify.delete('/users/:user_id/projects/:project_id', projectsController.delete)
 }
 
 module.exports = projectRoutes
