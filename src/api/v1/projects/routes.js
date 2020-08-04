cf06df59bca859b0239e53b79791b8e516b03cbb
FJKhan <fjgenus@gmail.com>
2020-08-03 22:02:18 -0400

    adds find (all) and findById routes

--- src/api/v1/projects/routes.js
+++ src/api/v1/projects/routes.js
@@ -1,5 +1,7 @@
 const projectsController = require('./controller')
 async function projectRoutes(fastify, options) {
+    fastify.get('/users/:user_id/projects/', projectsController.find)
+    fastify.get('/users/:user_id/projects/:project_id', projectsController.findById)
     fastify.post('/users/:user_id/projects/', projectsController.create)
     fastify.put('/users/:user_id/projects/:project_id', projectsController.update)
     fastify.delete('/users/:user_id/projects/:project_id', projectsController.delete)
