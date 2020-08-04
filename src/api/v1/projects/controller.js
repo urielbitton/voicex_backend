048d11bc007b178ce66a8b5f17dfd0a1c7e00010
FJKhan <fjgenus@gmail.com>
2020-08-03 21:06:00 -0400

    adds update and delete functions for projects dao

--- src/api/v1/projects/controller.js
+++ src/api/v1/projects/controller.js
@@ -1,13 +1,50 @@
-const { ObjectId } = require("mongodb")
+const { ObjectId } = require('mongodb')
 const projectsDAO = require('./dao')
-exports.create = async (req, res) =>{
+exports.create = async (req, res) => {
     try {
-        const  project  = req.body
-        const {user_id} = req.params
-        const data = Object.assign({}, {user_id: ObjectId(user_id)}, project, {last_modified: new Date()})
+        const project = req.body
+        const { user_id } = req.params
+        const data = Object.assign({}, { user_id: ObjectId(user_id) }, project, { last_modified: new Date() })
         await projectsDAO.create(data)
         res.send(data)
-    } catch(e) {
+    } catch (e) {
+        res.status(500)
+        res.send(e)
+        return
+    }
+}
+
+exports.update = async (req, res) => {
+    try {
+        const { project_id, user_id } = req.params
+        const { name } = req.body
+        const update = {
+            $currentDate: {
+                last_modified: true
+            },
+            $set: { name: name }
+        }
+        const result = await projectsDAO.update(project_id, user_id, update)
+        const { n, ok } = result.result
+        const { modifiedCount } = result
+        res.send({ n: n, ok: ok, modifiedCount: modifiedCount })
+        return
+    } catch (e) {
+        res.status(500)
+        res.send(e)
+        return
+    }
+}
+
+exports.delete = async (req,res) => {
+    try {
+        const { project_id, user_id } = req.params
+        const result = await projectsDAO.delete(project_id, user_id)
+        const { n, ok } = result.result
+        const { deletedCount } = result
+        res.send({ n: n, ok: ok, deletedCount: deletedCount })
+        return
+    } catch (e) {
         res.status(500)
         res.send(e)
         return
